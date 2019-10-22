33a5e28e9ff4629e0a5bba4c3eebc3e30968206b
Ricardo Perez <rp352429@gmail.com>
2019-10-22 10:49:04 -0500

    change port proxi

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var config = {
  		port: 'mongodb+srv://rp352429:rp352429@cluster0-psfrv.mongodb.net/test?retryWrites=true&w=majority'
  	},
  	token :{
- 		secret : 'coffee_garden_954395164264495_DYJ0jReUd3Ufhp5VAaPEzNL9aCA'
+ 		secret : 'coffee_garden_954395164264495_DYJ0jReUd3Ufhp5VAaPEzNL9aCA_'
  	},
  	cloudinary : {
 		name : 'coffe-garden',
