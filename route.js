01765cf72601b9ca157a53d2233159c9905b4709
Ricardo Perez <rp352429@gmail.com>
2019-10-21 21:46:09 -0500

    build react

--- route.js
+++ route.js
@@ -6,15 +6,15 @@ module.exports ={
     
     if (process.env.NODE_ENV === 'production') {
    
-        app.use(express.static(path.join(__dirname,'client', 'build')));
+        app.use(express.static('./public'));
         app.get('/*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
         });
 
     }else{
-        app.use(express.static('client/public'));
-        app.get('/', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+        app.use(express.static('./public'));
+        app.get('/*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
         });
     }
 
